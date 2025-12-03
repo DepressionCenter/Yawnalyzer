11db57165081dbcfa3a1411fb6766e9b4582251f
Brandon Labbree <blabbree@umich.edu>
2025-12-03 10:53:33 -0500

    Add new path to PathKeeper_template for data presence summary logs.

--- PathKeeper_template.py
+++ PathKeeper_template.py
@@ -2,3 +2,4 @@
 # completed_participant_path = "PATH/TO/MainData/Folder/completed_participants.txt"
 # incomplete_participant_path = "PATH/TO/MainData/Folder/issues_participants.txt"
 # merged_data_path = "PATH/TO/FOLDER/FOR/MERGED/DATA" 
+# data_log_path = "PATH/TO/FOLDER/FOR/LOGS"
