61b65b4e7189fdfbebfa29b6e355923b25c140f3
Brandon Labbree <blabbree@umich.edu>
2026-01-28 17:06:42 -0500

    Add in short explanation for PathKeeper_template.py and its purpose.

--- PathKeeper_template.py
+++ PathKeeper_template.py
@@ -1,3 +1,5 @@
+#This File is a template file to create a personalized PathKeeper.py for individual users to store user-specific paths.
+
 # inital_path = "PATH/TO/MainData/Folder"
 # completed_participant_path = "PATH/TO/MainData/Folder/completed_participants.txt"
 # incomplete_participant_path = "PATH/TO/MainData/Folder/issues_participants.txt"
